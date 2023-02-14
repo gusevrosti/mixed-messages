1f2e70d1529df26776541546bf763d3e0e22f935
Rosti Gusev <gusevrosti@proton.me>
2023-02-14 14:18:55 +1000

    add a constructJoke function that logs a random knock-knock joke to the console

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const setup = ` - Knock, knock. \n - Who's There?`;
+const jokeBodies = [
+  ['Nobel', `that's why I nocked.`],
+  ['Luke', 'through the peep hole and find out.'],
+  ['Figs', `the doorbell, it's not working!`],
+  ['Annie', 'thing you can do, I can do too!'],
+  ['Hal', `will you know if you don't opne the door?`],
+  ['Tank', `You're welcome.`],
+];
 
 /**
  * Chooses a random item from a passed array and returns it's value.
@@ -6,3 +13,20 @@ const setup = ` - Knock, knock. \n - Who's There?`;
  * @returns {string} a random item's value of a passed array.
  */
 const chooseRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+/**
+ * Constructs a random Knock-knock joke and logs it to the console.
+ */
+const constructJoke = () => {
+  const jokeBody = chooseRandomItem(jokeBodies);
+  const name = jokeBody[0];
+  const punchline = jokeBody[1];
+  const setup = ` - Knock, knock. \n - Who's There? \n - ${name}.`;
+  if (name === 'Tank') {
+    console.log(`${setup} \n - ${punchline}`);
+  } else {
+    console.log(`${setup} \n - ${name} ${punchline}`);
+  }
+}
+
+constructJoke();
